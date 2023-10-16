8da1842c1e7cc824ce5d74155101ef28f1c2dd82
sebousan <sebousan@gmail.com>
2023-10-16 10:58:06 +0200

    Simplify newsletter block

--- admin/app/blocks/newsletter.js
+++ admin/app/blocks/newsletter.js
@@ -15,35 +15,35 @@ export const block_newsletter = {
     fields: [
         heading,
         {
-            name: 'platform',
-            label: t.blocks.newsletter.fields.platform.label,
-            widget: 'select',
-            i18n: 'duplicate',
-            options: [
-                { label: t.blocks.newsletter.fields.platform.options.sendinblue, value: 'sendinblue' },
-                { label: t.blocks.newsletter.fields.platform.options.mailchimp, value: 'mailchimp' }
-            ],
-            default: 'mailchimp'
-        },
-        {
-            name: 'hiddens',
-            label: t.blocks.newsletter.fields.hiddens.label,
-            widget: 'list',
+            name: 'form',
+            label: t.blocks.newsletter.fields.form,
+            widget: 'object',
+            required: true,
             i18n: true,
-            collapsed: true,
-            required: false,
+            collapsed: false,
+            summary: '{{ url }}',
             fields: [
+                url,
                 {
-                    name: 'name',
-                    label: t.blocks.newsletter.fields.hiddens.fields.name,
-                    widget: 'string',
+                    name: 'hiddens',
+                    label: t.blocks.newsletter.fields.hiddens.label,
+                    widget: 'list',
+                    i18n: true,
+                    collapsed: true,
                     required: false,
-                    i18n: 'duplicate'
+                    fields: [
+                        {
+                            name: 'name',
+                            label: t.fields.name,
+                            widget: 'string',
+                            required: false,
+                            i18n: 'duplicate'
+                        },
+                        value
+                    ]
                 },
-                value
             ]
         },
-        url,
         background
     ]
 }
